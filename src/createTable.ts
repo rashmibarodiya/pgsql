a208e330738a5a6f269c93b770748501cab6a6d2
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-15 10:31:59 +0000

    checkpoint

---
+++ src/createTable.ts
@@ -0,0 +1,32 @@
+
+import getClient from "./utils";
+
+export async function createTable() {
+    console.log("i am here")
+    const userTableQuery = `
+    create TABLE user (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+    )`
+
+    const client = await getClient()
+
+    await client.query(userTableQuery)
+
+const createtodoTableQuery =`
+create TABLE todos (
+id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+description VARCHAR(255) NOT NULL,
+user_id INTEGER REFERENCES user(id)
+done BOOLEAN DEFAULT FALSE)
+`
+
+await client.query(createtodoTableQuery)
+
+
+console.log("table created successfully")
+}
+
+createTable()
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,13 @@
-
 import { Client } from "pg";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export default async function getClient() {
     const cl = new Client(process.env.uri!)
-    console.log(process.env.uri!)
+  //  console.log(process.env.uri!)
     await cl.connect()
     return cl;
-}
+ }
+
+//console.log(process.env.uri!)
