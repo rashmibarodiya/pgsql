e64c25d1059e53c61d4962d6813acb190b78283f
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-15 10:50:11 +0000

    updated

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,22 @@
 import { Client } from "pg";
 import dotenv from 'dotenv';
+import path from 'path'
+import fs from 'fs'
+
 dotenv.config();
 
 
 export default async function getClient() {
-    const cl = new Client(process.env.uri!)
+    const cacertpath = path.resolve(__dirname,'../cert/ca.pem')
+    const cacert = fs.readFileSync(cacertpath).toString()
+
+    const cl = new Client({
+        connectionString : process.env.uri,
+        ssl:{
+            ca : cacert,
+            rejectUnauthorized :true
+        }
+    })
   //  console.log(process.env.uri!)
     await cl.connect()
     return cl;
