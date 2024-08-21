10ee6c4ade51725c50204559bfc7a90323212ef7
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-21 15:44:17 +0000

    changed structure

--- postgreSql/src/insert.ts
+++ postgreSql/src/insert.ts
@@ -5,14 +5,14 @@ import getClient from "./utils"
 export async function insertTodo(){
 
      const client = await getClient()
-    // const userText = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id`;
-    // const userVal = [`me@gmail.com`, "my password"];
-    // let resposnse = await client.query(userText,userVal)
+    const userText = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id`;
+    const userVal = [`test@gmail.com`, "my password"];
+    let resposnse = await client.query(userText,userVal)
 
     const todoText = `INSERT INTO todos (title, description, user_id, done)
     values ($1, $2, $3, $4) RETURNING id`;
 
-    const todoVal = ["complete kdhfkj", "des ",3, false ];
+    const todoVal = ["test", "des ",3, false ];
     await client.query(todoText, todoVal);
     console.log("entries created successfully!!")
 }
