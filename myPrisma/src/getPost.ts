d2a20f64fab5fe9f4230decbae20729d7478f4a8
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-22 13:01:39 +0000

    getPost fixed

--- myPrisma/src/getPost.ts
+++ myPrisma/src/getPost.ts
@@ -2,20 +2,24 @@
 import { PrismaClient } from "@prisma/client";
 
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({log: ['info', 'query']})
 async function main() {
     // const res = await prisma.post.findMany({})
     // console.log(res)
     // console.log("*******************************")
 
     const post = await prisma.post.findMany({
-    //    include : {
-    //     author : {
-    //         select : {
-    //             publis
-    //         }
-    //     }
-    //    }
+        where : {
+            authorId : 1
+        },
+        include: {
+            author: {
+                select: {
+                    email : true,
+                    name :true
+                }
+            }
+        }
     })
     console.log(post)
 
